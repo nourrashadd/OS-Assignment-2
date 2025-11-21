8af065dd8df91b3e45b4039b6b1063daf10adb26
Nour Rashad <nour.mohamed.2024@aiu.edu.eg>
2025-11-21 14:39:58 +0200

    OS Assignment2

--- fork.c
+++ fork.c
@@ -5,13 +5,11 @@ int main() {
     pid_t pid = fork();
 
     if (pid == 0) {
-        // Child process
         printf("This is the child process. PID: %d\n", getpid());
     } else if (pid > 0) {
-        // Parent process
         printf("This is the parent process. PID: %d\n", getpid());
     } else {
-        // Error
+        
         printf("Fork failed!\n");
     }
 
